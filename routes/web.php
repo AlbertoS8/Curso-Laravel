4488fd3d9cd4f5b6a12b6b062a482b4528f80891
Jose Alberto Velasco <josealbertovelasco@MacBook-Pro-de-Ing-303.local>
2024-02-14 15:13:51 -0700

    Laravel Sanctum

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('welcome');
 });
 
 Route::group(['prefix' => 'dashboard', 'middleware' => ['auth', "admin"]], function (){
@@ -60,4 +60,7 @@ Route::group(['prefix' => 'blog'],function () {
     Route::get('/{post}', "show")->name("web.blog.show");
    }); 
 });
+
+
+
 require __DIR__.'/auth.php';
